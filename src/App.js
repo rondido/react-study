70c3a35fee4f265421d439f5f5bb65b9d70d291f
Park <rppr001@naver.com>
2021-10-20 21:17:42 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={date: new Date()};
+  }
+
+  //생명주기 메서드
+  componentDidMount(){
+    this.timerID = setInterval(
+      ()=> this.tick(),
+      1000
+    );
+  }
+  componentWillUnmount(){
+    clearInterval(this.timerID);
+  }
+  tick(){
+    this.setState({
+      date:new Date()
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
 }
-
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
 export default App;
